20f715bef4a4213dab0253b2b530180db3b62a04
YBasile-mclr <basile.mauclaire@gmail.com>
2025-04-10 15:17:34 +0200

    updtae

--- Trophii/app.R
+++ Trophii/app.R
@@ -1,5 +1,5 @@
 # Réinstaller les packages nécessaires
-install.packages(c("shiny", "bslib", "rsconnect"))
+
 
 # Charger les packages
 library(shiny)
@@ -24,4 +24,4 @@ rsconnect::setAccountInfo(name='wcpym7-basile-mauclaire',
 			  token='04CCCFBA57D8149243B59B24212B05B6',
 			  secret='jT8GK0hW6/fR6A3ewkCnv6JkOzeEgD9ZfdpeNjzR')
 
-rsconnect::deployApp("https://github.com/Basile-mclr/Biodiv/tree/main/Trophii")
+rsconnect::deployApp('C:\\Users\\J1149071\\Biodiv\\Trophii')
